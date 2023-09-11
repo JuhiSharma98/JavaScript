247ef98badf8b631248be911d3840ec8fc6befc3
Juhi Sharma <60868588+JuhiSharma98@users.noreply.github.com>
2023-09-11 05:17:45 +0000

    datatype commit

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,22 @@
+// Primitive
+//symbol - makes the value unique
+ const id = Symbol('123')
+ const anotherId = Symbol('123')
+
+ console.log(id === anotherId);
+
+ //arrays
+ const heros =["App", "natraj", "doga"]
+ //obj
+ let myobj= {
+    name: "juhi",
+    age: 25,
+ }
+
+ //function
+ const myFunction = function()
+ {
+    console.log("Hello");
+ }
+
+ 
