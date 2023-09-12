bbc4487b861c1adf68778690b7526bae2c98fff8
Juhi Sharma <60868588+JuhiSharma98@users.noreply.github.com>
2023-09-12 06:08:05 +0000

    object destr commit

---
+++ .devcontainer/02_basics/04_objects.js
@@ -0,0 +1,27 @@
+//destructuring of objects
+
+const course = {
+    courseName:"js",
+    price: "999",
+    courseInst: "ju",
+}
+
+const {courseInst: inst} =course //renaming- deconstruction
+console.log(inst);
+
+
+//destructuring sysntax
+/*const navbar = ({company}) => {
+
+}
+
+navbar(company = "hitesh")*/
+
+//accepted as json
+{
+    name: "juhi"
+    coursename: "js"
+    price: "free"
+}
+
+
