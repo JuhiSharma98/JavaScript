4da00b0a4a8e885b7a909fc979bc4b8aeaf8a290
Juhi Sharma <60868588+JuhiSharma98@users.noreply.github.com>
2023-09-14 06:10:31 +0000

    new array commit

--- .devcontainer/02_basics/01_arrays.js
+++ .devcontainer/02_basics/01_arrays.js
@@ -1,4 +1,13 @@
-/arrays
+//arrays
 
 const myArray = [0,1,1,2,3,4]
 console.log(myArray[2]);
+
+//Array methods
+
+//myArray.push(9)
+//myArray.push(78)
+//myArray.pop()  //pop out last value
+//myArray.unshift(4)
+myArray.shift()
+ console.log(myArray);
